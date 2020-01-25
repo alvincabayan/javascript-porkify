f37b1f436c41c537076c4a5166c0133d14ffb92a
Alvin Cabayan <alvincabayan@gmail.com>
2020-01-26 00:13:18 +1300

    Start up to Building the recipe model

---
+++ src/js/models/Search.js
@@ -0,0 +1,24 @@
+// NOT: const res = await axios(`${PROXY}http://food2fork.com/api/search?key=${KEY}&q=${this.query}`);
+// USE: const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+import axios from 'axios';
+
+export default class Search {
+    constructor(query) {
+        this.query = query;
+    }
+
+    async getResults() {
+        try {
+            const res = await axios(`https://forkify-api.herokuapp.com/api/search?&q=${this.query}`);
+            this.result = res.data.recipes;
+            // console.log(this.result);
+        } catch (error) {
+            alert(error);
+        }
+        
+    }
+}
+
+
+
+
